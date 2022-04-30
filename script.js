57bd31307168a8d5c4d3f886ab578d13e9345a26
Rahul Kumar Mishra <rkm.rahulkmishra@gmail.com>
2022-04-30 22:28:12 +0530

    nav-bar implimentation

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log("hello");
+const navToggle = document.querySelector(".navbar_toggle");
+const links = document.querySelector(".main_nav");
+
+navToggle.addEventListener('click', function(){
+    links.classList.toggle("show_nav");
+})
