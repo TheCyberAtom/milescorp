73be64f711d92c28ca951f212b6e8ffb74bfb5b1
Rahul Kumar Mishra <rkm.rahulkmishra@gmail.com>
2022-05-01 16:49:46 +0530

    small fixes

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const navToggle = document.querySelector(".navbar_toggle");
 const links = document.querySelector(".main_nav");
+const nav_links = document.querySelector('.nav_links');
 
 navToggle.addEventListener('click', function(){
     links.classList.toggle("show_nav");
+    console.log(nav_links);
+    nav_links.addEventListener('click', ()=>{
+        links.classList.remove("show_nav");
+    })
 })
