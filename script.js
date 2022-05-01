db974194c017d17b8ad7ffa43ceb4f8122dda314
Rahul Kumar Mishra <rkm.rahulkmishra@gmail.com>
2022-05-01 18:44:15 +0530

    link and hover update

--- script.js
+++ script.js
@@ -9,3 +9,23 @@ navToggle.addEventListener('click', function(){
         links.classList.remove("show_nav");
     })
 })
+
+
+
+// Scroll to top button
+
+var mybutton = document.getElementById("myBtn");
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
